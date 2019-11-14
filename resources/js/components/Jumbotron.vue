df65d16678ad498ce62cd78a990ddbbca02c9c4b
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-11-14 22:59:03 +0100

    Added 'why us' section.

---
+++ resources/js/components/Jumbotron.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Jumbotron"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/RecentProperties.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "RecentProperties"
+    }
+</script>
+
+<style scoped>
+
+</style>
