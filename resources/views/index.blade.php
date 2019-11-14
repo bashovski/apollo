e724e7dfcb69ba3161a17e54ee2e95532828a58f
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-11-14 19:49:48 +0100

    Added property visits in recent-properties items.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -136,19 +136,34 @@
         <h2 class="apollo_heading2 d-flex justify-content-center">Recently added</h2>
         <div class="row mt-5">
             <div class="col">
-                <img class="apollo_recent_item_image" src="/img/examplum1.jpg" height="200" width="312" alt="">
+                <div class="apollo_recent_item" style="background-image: url(/img/examplum1.jpg)">
+                    <div class="apollo_recent_item_visits">
+                        <img src="/svg/apollo_visits.svg" width="18" height="18" alt="">
+                        <span style="padding-left: 4px;">3.4k</span>
+                    </div>
+                </div>
                 <h6 class="apollo_recent_item_heading">Eperweg 13</h6>
                 <p class="apollo_recent_item_city">7313 AV Apeldoorn</p>
                 <h6 class="apollo_recent_item_price">€ 99 000 000</h6>
             </div>
             <div class="col">
-                <img class="apollo_recent_item_image" src="/img/examplum2.jpg" height="200" width="312" alt="">
+                <div class="apollo_recent_item" style="background-image: url(/img/examplum2.jpg)">
+                    <div class="apollo_recent_item_visits">
+                        <img src="/svg/apollo_visits.svg" width="18" height="18" alt="">
+                        <span style="padding-left: 4px;">3.4k</span>
+                    </div>
+                </div>
                 <h6 class="apollo_recent_item_heading">Eperweg 13</h6>
                 <p class="apollo_recent_item_city">7313 AV Apeldoorn</p>
                 <h6 class="apollo_recent_item_price">€ 99 000 000</h6>
             </div>
             <div class="col">
-                <img class="apollo_recent_item_image" src="/img/examplum3.jpg" height="200" width="312" alt="">
+                <div class="apollo_recent_item" style="background-image: url(/img/examplum3.jpg)">
+                    <div class="apollo_recent_item_visits">
+                        <img src="/svg/apollo_visits.svg" width="18" height="18" alt="">
+                        <span style="padding-left: 4px;">3.4k</span>
+                    </div>
+                </div>
                 <h6 class="apollo_recent_item_heading">Eperweg 13</h6>
                 <p class="apollo_recent_item_city">7313 AV Apeldoorn</p>
                 <h6 class="apollo_recent_item_price">€ 99 000 000</h6>
