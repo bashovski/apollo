d4e3853c018a6b42f06467a72ad099798aba1c6c
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-11-15 00:37:35 +0100

    Added navbar file as a layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&amp;key=AIzaSyCiGbsG4L3k5s4GRHLtklAXEI9EyREJzEI"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -23,35 +24,7 @@
 </head>
 <body>
     <div id="app">
-        <nav id="apollo_nav" class="navbar navbar-light navbar-expand-md fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#" style="color: #fff;">
-                    <img src="/img/apollo_logo.png" width="42" height="42" alt="">
-                    <span id="apollo_nav_brand_span">APOLLO</span>
-                </a>
-                <button data-toggle="collapse" class="navbar-toggler" data-target="#"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse"
-                     href="#" style="color: #fff;" ver="">
-                    <ul class="nav navbar-nav ml-auto">
-                        <li class="apollo_nav_item nav-item" role="presentation"><a class="nav-link" href="#" style="color: #4AD7D1;padding-right: 30px;">
-                                <img id="apollo_nav_main_btn_pin" src="/svg/apollo_account.svg" height="18" width="18" alt="">
-                                <span>My Account</span>
-                            </a>
-                        </li>
-                        <li class="apollo_nav_item nav-item" role="presentation"><a class="nav-link" href="#">
-                                <img id="apollo_nav_main_btn_pin" src="/svg/apollo_properties.svg" height="18" width="18" alt="">
-                                <span>My Properties</span>
-                            </a>
-                        </li>
-                        <li class="nav-item" role="presentation"><a class="nav-link" href="#" id="apollo_nav_main_btn">
-                                <img id="apollo_nav_main_btn_pin" src="/svg/apollo_pin.svg" height="18" width="18" alt="">
-                                <span>Place a Property</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
+        @include('layouts.navbar')
         <main id="apollo_main">
             @yield('content')
         </main>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav id="apollo_nav" class="navbar navbar-light navbar-expand-md fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#" style="color: #fff;">
+            <img src="/img/apollo_logo.png" width="42" height="42" alt="">
+            <span id="apollo_nav_brand_span">APOLLO</span>
+        </a>
+        <button data-toggle="collapse" class="navbar-toggler" data-target="#"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse"
+             href="#" style="color: #fff;" ver="">
+            <ul class="nav navbar-nav ml-auto">
+                <li class="apollo_nav_item nav-item" role="presentation"><a class="nav-link" href="#" style="color: #4AD7D1;padding-right: 30px;">
+                        <img id="apollo_nav_main_btn_pin" src="/svg/apollo_account.svg" height="18" width="18" alt="">
+                        <span>My Account</span>
+                    </a>
+                </li>
+                <li class="apollo_nav_item nav-item" role="presentation"><a class="nav-link" href="#">
+                        <img id="apollo_nav_main_btn_pin" src="/svg/apollo_properties.svg" height="18" width="18" alt="">
+                        <span>My Properties</span>
+                    </a>
+                </li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="#" id="apollo_nav_main_btn">
+                        <img id="apollo_nav_main_btn_pin" src="/svg/apollo_pin.svg" height="18" width="18" alt="">
+                        <span>Place a Property</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
