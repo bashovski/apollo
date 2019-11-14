b909fc6b1a27423bb969a39be6138e6550fce88d
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-11-14 02:35:00 +0100

    Added initial look with all the content of the 'search for properties' container.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -64,5 +64,72 @@
                 <span>Newly built</span>
             </div>
         </div>
+        <div id="apollo_search_properties_container">
+            <form id="apollo_search_fields" action="" method="post" enctype="multipart/form-data">
+                @csrf
+                <div id="apollo_search_left_half">
+                    <input id="apollo_search_field" placeholder="Address, City, State, etc." type="text" class="form-control">
+                    <div class="dropdown show" style="width: 20%;">
+                        <a id="apollo_dropdown_button" class="btn dropdown-toggle" href="#" role="button" data-toggle="dropdown" >
+                            <span style="vertical-align: middle;">+ 0 km</span>
+                            <img src="/svg/apollo_down_caret.svg" height="14" width="14" style="vertical-align: middle;" alt="">
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item">+ 1 km</a>
+                            <a class="dropdown-item">+ 2 km</a>
+                            <a class="dropdown-item">+ 5 km</a>
+                            <a class="dropdown-item">+ 10 km</a>
+                            <a class="dropdown-item">+ 25 km</a>
+                            <a class="dropdown-item">+ 50 km</a>
+                            <a class="dropdown-item">+ 100 km</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="apollo_search_right_half">
+                    <div class="dropdown show" style="width: 30%;">
+                        <a id="apollo_dropdown_button" class="btn dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                           style="border-radius: 3px 0 0 3px;">
+                            <span style="vertical-align: middle;">From</span>
+                            <span style="vertical-align: middle;">€ 0</span>
+                            <img src="/svg/apollo_down_caret.svg" height="14" width="14" style="vertical-align: middle;" alt="">
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item">€ 0</a>
+                            <a class="dropdown-item">€ 25 000</a>
+                            <a class="dropdown-item">€ 50 000</a>
+                            <a class="dropdown-item">€ 100 000</a>
+                            <a class="dropdown-item">€ 250 000</a>
+                            <a class="dropdown-item">€ 500 000</a>
+                            <a class="dropdown-item">€ 750 000</a>
+                            <a class="dropdown-item">€ 1 000 000</a>
+                            <a class="dropdown-item">€ Over a million</a>
+                        </div>
+                    </div>
+                    <div class="dropdown show" style="width: 30%; margin-left: 3px">
+                        <a id="apollo_dropdown_button" class="btn dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                           style="border-radius: 0 3px 3px 0;">
+                            <span style="vertical-align: middle;">To</span>
+                            <span style="vertical-align: middle;">€ 0</span>
+                            <img src="/svg/apollo_down_caret.svg" height="14" width="14" style="vertical-align: middle;" alt="">
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item">€ 0</a>
+                            <a class="dropdown-item">€ 25 000</a>
+                            <a class="dropdown-item">€ 50 000</a>
+                            <a class="dropdown-item">€ 100 000</a>
+                            <a class="dropdown-item">€ 250 000</a>
+                            <a class="dropdown-item">€ 500 000</a>
+                            <a class="dropdown-item">€ 750 000</a>
+                            <a class="dropdown-item">€ 1 000 000</a>
+                            <a class="dropdown-item">€ Over a million</a>
+                        </div>
+                    </div>
+                    <button id="apollo_search_button" class="apollo_btn_primary" type="button">
+                        <img class="apollo_search_filter_icon" height="18" width="18" src="/svg/apollo_search_sm.svg">
+                        <span>Search</span>
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
