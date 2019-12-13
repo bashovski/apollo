3365b7c0767d26868d4668a62392710a56e6711c
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-12-13 22:54:09 +0100

    Add property model and up/drop migration

--- app/Property.php
+++ app/Property.php
@@ -4,7 +4,42 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Property extends Model
-{
-    //
+class Property extends Model {
+    protected $fillable = [
+        'seller_id',
+        'agent_id',
+        'agent_confirmation_id',
+        'status',
+        'transaction_type',
+        'purchase_id',
+        'photos',
+        'address',
+        'postcode',
+        'year_of_construction',
+        'living_area',
+        'rooms_num',
+        'bathrooms_num',
+        'toilets_num',
+        'bedrooms_num',
+        'has_basements',
+        'has_pool',
+        'energy_label',
+        'insulation',
+        'heating',
+        'hot_water',
+        'boiler',
+        'cadastre_id',
+        'cadastre_registered_area',
+        'cadastre_ownership',
+        'has_shed',
+        'plot_size',
+        'price',
+        'has_garage',
+        'garage_type',
+        'garage_cap',
+        'nearby_places',
+        'brochure_id',
+        'description',
+        'page_visits'
+    ];
 }
--- database/migrations/2019_11_12_185515_create_properties_table.php
+++ database/migrations/2019_11_12_185515_create_properties_table.php
@@ -15,6 +15,41 @@ class CreatePropertiesTable extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('seller_id')->nullable();
+            $table->unsignedBigInteger('agent_id')->nullable();
+            $table->unsignedBigInteger('agent_confirmation_id')->nullable();
+            $table->string('status')->default('');
+            $table->string('transaction_type')->nullable();
+            $table->unsignedBigInteger('purchase_id')->nullable();
+            $table->json('photos')->nullable();
+            $table->string('address')->nullable();
+            $table->string('postcode')->nullable();
+            $table->unsignedSmallInteger('year_of_construction')->nullable();
+            $table->unsignedInteger('living_area')->nullable();
+            $table->unsignedSmallInteger('rooms_num')->nullable();
+            $table->unsignedSmallInteger('bathrooms_num')->nullable();
+            $table->unsignedSmallInteger('toilets_num')->nullable();
+            $table->unsignedSmallInteger('bedrooms_num')->nullable();
+            $table->boolean('has_basement')->nullable();
+            $table->boolean('has_pool')->nullable();
+            $table->char('energy_label')->nullable();
+            $table->string('insulation')->nullable();
+            $table->string('heating')->nullable();
+            $table->string('hot_water')->nullable();
+            $table->string('boiler')->nullable();
+            $table->string('cadastre_id')->nullable();
+            $table->string('cadastre_registered_area')->nullable();
+            $table->string('cadastre_ownership')->nullable();
+            $table->boolean('has_shed')->nullable();
+            $table->unsignedInteger('plot_size')->nullable();
+            $table->unsignedInteger('price')->nullable();
+            $table->boolean('has_garage')->nullable();
+            $table->string('garage_type')->nullable();
+            $table->unsignedSmallInteger('garage_cap')->nullable();
+            $table->json('nearby_places')->nullable();
+            $table->string('brochure_id')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('page_visits')->default(0);
             $table->timestamps();
         });
     }
